3d4c356768cf1780d089f3dd46194d0c34af7800
joaothompson <37004063+joaothompson@users.noreply.github.com>
2025-03-26 06:54:46 -0300

    Update test.c

--- test.c
+++ test.c
@@ -5,7 +5,7 @@ int main(void)
 {
   int number1, reverseNumber;
   scanf("%d", &number1);
-  total = reverse(number1);
+  reverseNumber = reverse(number1);
   printf("%d\n", reverseNumber);
   return 0;
 }
